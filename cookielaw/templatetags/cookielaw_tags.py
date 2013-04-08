62bd34de58f3478174a52eec3ec441f27886fa5f
Piotr Kilczuk <piotr@tymaszweb.pl>
2013-04-08 12:17:01 +0200

    Add cookielaw_banner template tag

---
+++ cookielaw/templatetags/cookielaw_tags.py
@@ -0,0 +1,23 @@
+from classytags.helpers import InclusionTag
+from django import template
+from django.template.loader import render_to_string
+
+
+register = template.Library()
+
+
+class CookielawBanner(InclusionTag):
+    """
+    Displays cookie law banner only if user has not dismissed it yet.
+    """
+
+    template = 'cookielaw/banner.html'
+
+    def render_tag(self, context, **kwargs):
+        template = self.get_template(context, **kwargs)
+        if context['request'].COOKIES.get('cookielaw_accepted', False):
+            return ''
+        data = self.get_context(context, **kwargs)
+        return render_to_string(template, data)
+
+register.tag(CookielawBanner)
