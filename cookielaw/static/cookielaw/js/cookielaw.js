512ae25399c52de89d982896453f11f211e5fcb5
Piotr Kilczuk <piotr@tymaszweb.pl>
2013-05-10 10:57:39 +0200

    JS code improve

--- cookielaw/static/cookielaw/js/cookielaw.js
+++ cookielaw/static/cookielaw/js/cookielaw.js
@@ -1,23 +1,25 @@
 var Cookielaw = {
 
     createCookie: function (name, value, days) {
+        var date = new Date(),
+            expires = '';
         if (days) {
-            var date = new Date();
-            date.setTime(date.getTime()+(days*24*60*60*1000));
-            var expires = "; expires="+date.toGMTString();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
         }
-        else var expires = "";
         document.cookie = name + "=" + value + expires + "; path=/";
     },
 
     createCookielawCookie: function () {
-        this.createCookie('cookielaw_accepted', '1', 10*365);
+        this.createCookie('cookielaw_accepted', '1', 10 * 365);
 
-        if (typeof(window.jQuery) === 'function') {
+        if (typeof (window.jQuery) === 'function') {
             jQuery('#CookielawBanner').slideUp();
         } else {
             document.getElementById('CookielawBanner').style.display = 'none';
         }
     }
 
-}
+};
