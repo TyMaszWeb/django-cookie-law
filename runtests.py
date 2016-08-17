9ca12c214fce1665973c9e3338b535a985a4007e
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-08-17 17:52:28 +0100

    Split settings

--- runtests.py
+++ runtests.py
@@ -3,38 +3,69 @@ from __future__ import absolute_import
 import os
 import sys
 
+import django
 from django.conf import settings
 from django.core import management
 
 
 def main():
-    settings.configure(
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-            }
-        },
-        INSTALLED_APPS = (
-            'django.contrib.staticfiles',
-            'cookielaw',
-            'cookielaw.test_project.test_app',
-        ),
-        MIDDLEWARE_CLASSES = (
-            'django.middleware.common.CommonMiddleware',
-        ),
-        TEMPLATE_CONTEXT_PROCESSORS = (
-            'django.contrib.auth.context_processors.auth',
-            'django.core.context_processors.debug',
-            'django.core.context_processors.i18n',
-            'django.core.context_processors.media',
-            'django.core.context_processors.request',
-            'django.core.context_processors.static',
-            'django.core.context_processors.tz',
-            'django.contrib.messages.context_processors.messages'
-        ),
-        ROOT_URLCONF = 'cookielaw.test_project.urls',
-        STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'cookielaw', 'static'),
-        STATIC_URL = '/static/',
-    )
-    management.execute_from_command_line(['', 'test', 'cookielaw',])
-    sys.exit()
+
+    if django.VERSION < (1, 9):
+        settings.configure(
+            DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                }
+            },
+            INSTALLED_APPS = (
+                'django.contrib.staticfiles',
+                'cookielaw',
+                'cookielaw.test_project.test_app',
+            ),
+            MIDDLEWARE_CLASSES = (
+                'django.middleware.common.CommonMiddleware',
+            ),
+            TEMPLATE_CONTEXT_PROCESSORS = (
+                'django.contrib.auth.context_processors.auth',
+                'django.core.context_processors.debug',
+                'django.core.context_processors.i18n',
+                'django.core.context_processors.media',
+                'django.core.context_processors.request',
+                'django.core.context_processors.static',
+                'django.core.context_processors.tz',
+                'django.contrib.messages.context_processors.messages'
+            ),
+            ROOT_URLCONF = 'cookielaw.test_project.urls',
+            STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'cookielaw', 'static'),
+            STATIC_URL = '/static/',
+        )
+        management.execute_from_command_line(['', 'test', 'cookielaw',])
+        sys.exit()
+
+    else:
+        settings.configure(
+            DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                }
+            },
+            INSTALLED_APPS = (
+                'django.contrib.staticfiles',
+                'cookielaw',
+                'cookielaw.test_project.test_app',
+            ),
+            MIDDLEWARE_CLASSES = (
+                'django.middleware.common.CommonMiddleware',
+            ),
+            TEMPLATES=[
+                {
+                    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                    'APP_DIRS': True,
+                },
+            ],
+            ROOT_URLCONF = 'cookielaw.test_project.urls',
+            STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'cookielaw', 'static'),
+            STATIC_URL = '/static/',
+        )
+        management.execute_from_command_line(['', 'test', 'cookielaw',])
+        sys.exit()
