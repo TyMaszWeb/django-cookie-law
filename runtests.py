33d4bc74213e957893f60bb667fe60a9d0edb9af
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-08-18 11:22:41 +0100

    Update settings.TEMPLATES

--- runtests.py
+++ runtests.py
@@ -61,6 +61,17 @@ def main():
                 {
                     'BACKEND': 'django.template.backends.django.DjangoTemplates',
                     'APP_DIRS': True,
+                    'OPTIONS': {
+                        'context_processors': [
+                            'django.contrib.auth.context_processors.auth',
+                            'django.template.context_processors.debug',
+                            'django.template.context_processors.i18n',
+                            'django.template.context_processors.media',
+                            'django.template.context_processors.static',
+                            'django.template.context_processors.tz',
+                            'django.contrib.messages.context_processors.messages',
+                        ],
+                    },
                 },
             ],
             ROOT_URLCONF = 'cookielaw.test_project.urls_2',
