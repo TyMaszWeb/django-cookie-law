5910a7da08ff50d406ec63c87e5ae516e63802fa
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-08-18 13:53:36 +0100

    A separate project for Django 1.4

---
+++ test_projects/test_project_14/manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '..'))
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project_14.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
