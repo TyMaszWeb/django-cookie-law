fb7ee1f0451b3e14bb7d0a20413de57fc51a803f
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-11-14 17:54:40 +0000

    Split tests

--- cookielaw/tests.py
+++ cookielaw/tests.py
@@ -15,8 +15,27 @@ class FunctionalTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_banner_shows_and_hides(self):
-        # first, without jQuery
+    def test_banner_shows_and_hides_jquery(self):
+        self.browser.get('http://localhost:8081/?jquery=1')
+        cookielaw_banner = self.browser.find_element_by_id('CookielawBanner')
+
+        # contains header, paragraph and agree button
+        self.assertIn(_('COOKIE_INFO_HEADER'), cookielaw_banner.text)
+        self.assertIn(_('COOKIE_INFO_PARA'), cookielaw_banner.text)
+        self.assertIn(_('COOKIE_INFO_OK'), cookielaw_banner.text)
+
+        # on click of the button, cookie set and banner hidden
+        cookielaw_banner.find_element_by_class_name('btn').click()
+        time.sleep(1)
+        self.assertFalse(cookielaw_banner.is_displayed())
+        self.assertEqual(self.browser.get_cookie('cookielaw_accepted')['value'], '1')
+
+        # on come back, assert banner gone
+        self.browser.get('http://localhost:8081/?jquery=1')
+        self.assertRaises(NoSuchElementException,
+            lambda: self.browser.find_element_by_id('CookielawBanner'))
+
+    def test_banner_shows_and_hides_no_jquery(self):
         self.browser.get('http://localhost:8081/')
         cookielaw_banner = self.browser.find_element_by_id('CookielawBanner')
 
@@ -34,15 +53,3 @@ class FunctionalTest(LiveServerTestCase):
         self.browser.get('http://localhost:8081/')
         self.assertRaises(NoSuchElementException,
             lambda: self.browser.find_element_by_id('CookielawBanner'))
-
-        self.browser.delete_cookie('cookielaw_accepted')
-
-        # now, with jQuery
-        self.browser.get('http://localhost:8081/?jquery=1')
-        cookielaw_banner = self.browser.find_element_by_id('CookielawBanner')
-
-        # on click of the button, cookie set and banner hidden
-        cookielaw_banner.find_element_by_class_name('btn').click()
-        time.sleep(1)
-        self.assertFalse(cookielaw_banner.is_displayed())
-        self.assertEqual(self.browser.get_cookie('cookielaw_accepted')['value'], '1')
