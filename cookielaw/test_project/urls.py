f96ee59c308a63041f4b1f75e94f07ca0bf4c802
Daniel Widerin <daniel@widerin.net>
2017-05-30 21:00:30 +0200

    Switch to pytest

--- cookielaw/test_project/urls.py
+++ cookielaw/test_project/urls.py
@@ -1,16 +1,13 @@
 from __future__ import absolute_import
 
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views import static
-
-
 from .test_app.views import HomeView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', HomeView.as_view()),
-
     # required since Django 1.7
     url(r'^static', static.serve, {'document_root': settings.STATIC_ROOT}),
-)
+]
---
+++ settings.py
@@ -0,0 +1,49 @@
+import os
+
+SECRET_KEY = 'test'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cookielaw',
+    'cookielaw.test_project.test_app',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+)
+ROOT_URLCONF = 'cookielaw.test_project.urls'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'cookielaw', 'static')
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
