b8b4ec704f1418fb3a45aa3ea4804a5f54065ecf
Piotr Kilczuk <piotr@kilczuk.dev>
2022-03-11 10:05:22 +0100

    Do not depend on jQuery at all

--- cookielaw/static/cookielaw/js/cookielaw.js
+++ cookielaw/static/cookielaw/js/cookielaw.js
@@ -1,41 +1,41 @@
 var Cookielaw = {
-    ACCEPTED: '1',
-    REJECTED: '0',
+  ACCEPTED: '1',
+  REJECTED: '0',
 
-    createCookie: function (name, value, days, secure=true) {
-        var date = new Date(),
-            expires = '';
-        if (days) {
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toGMTString();
-        } else {
-            expires = "";
-        }
-        
-        var secureString = "";
-        if (window.isSecureContext && secure) {
-            secureString = "; Secure";
-        }
-        
-        document.cookie = name + "=" + value + expires + "; path=/" + secureString;
-    },
-
-    createCookielawCookie: function (cookieValue, secure=true) {
-        cookieValue = cookieValue || this.ACCEPTED;
-        this.createCookie('cookielaw_accepted', cookieValue, 10 * 365, secure);
-
-        if (typeof (window.jQuery) === 'function') {
-            jQuery('#CookielawBanner').slideUp();
-        } else {
-            document.getElementById('CookielawBanner').style.display = 'none';
-        }
-    },
-
-    accept: function () {
-        this.createCookielawCookie(this.ACCEPTED);
-    },
-
-    reject: function () {
-          this.createCookielawCookie(this.REJECTED);
+  createCookie: function (name, value, days, secure = true) {
+    var date = new Date(),
+      expires = '';
+    if (days) {
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    } else {
+      expires = "";
     }
+
+    var secureString = "";
+    if (window.isSecureContext && secure) {
+      secureString = "; Secure";
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/" + secureString;
+  },
+
+  setCookielawCookie: function (cookieValue, secure = true) {
+    cookieValue = cookieValue || this.ACCEPTED;
+    this.createCookie('cookielaw_accepted', cookieValue, 10 * 365, secure);
+  },
+
+  hideCookielawBanner: function () {
+    document.getElementById('CookielawBanner').style.display = 'none';
+  },
+
+  accept: function () {
+    this.setCookielawCookie(this.ACCEPTED);
+    this.hideCookielawBanner();
+  },
+
+  reject: function () {
+    this.setCookielawCookie(this.REJECTED);
+    this.hideCookielawBanner();
+  }
 };
